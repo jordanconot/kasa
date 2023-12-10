6c715dbfc016d11850702a7b1e93bab8c21bda3d
unknown <jordan.conot@gmail.com>
2023-12-10 16:42:58 +0100

    feat: add underline header link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const Header = () => {
+  const location = useLocation();
+  const isHome = location.pathname === '/';
+  const isAbout = location.pathname === '/a-propos';
+
   return (
     <header className="header_container">
       <div className="header_wrapper_logo">
         <div className="header_logo"></div>
       </div>
       <div className="header_nav">
-        <Link to="/" className="header_text_nav ">
+        <Link to="/" className={`header_text_nav ${isHome ? 'active' : ''}`}>
           Accueil
         </Link>
-        <Link to="/a-propos" className="header_text_nav">
+        <Link to="/a-propos" className={`header_text_nav ${isAbout ? 'active' : ''}`}>
           A propos
         </Link>
       </div>
