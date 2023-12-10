639c048e69a2a0cdbde8e680afbab4d6709504ad
unknown <jordan.conot@gmail.com>
2023-12-10 17:11:28 +0100

    feat: add img + title from file json

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,34 +1,27 @@
 import React from 'react';
+import data from '../data/data.json';
 
 const Card = () => {
-    return (
-        <main className='main_container'>
-            <section className='card_container'>
-                <div className='card_list'>
-                    <a href=''>
-                        <div className='card_location'>
-                            <img className='card_location_picture' alt=''></img>
-                            <h2 className='title_n2'>Titre de la location</h2>
-                        </div>
-                    </a>
-
-                    <a href=''>
-                        <div className='card_location'>
-                            <img className='card_location_picture' alt=''></img>
-                            <h2 className='title_n2'>Titre de la location</h2>
-                        </div>
-                    </a>
-
-                    <a href=''>
-                        <div className='card_location'>
-                            <img className='card_location_picture' alt=''></img>
-                            <h2 className='title_n2'>Titre de la location</h2>
-                        </div>
-                    </a>
-                </div>
-            </section>
-        </main>
-    );
+  return (
+    <main className="main_container">
+      <section className="card_container">
+        <div className="card_list">
+          {data.map((item) => (
+            <a href="" key={item.id}>
+              <div className="card_location">
+                <img
+                  src={item.cover}
+                  className="card_location_picture"
+                  alt={item.title}
+                ></img>
+                <h2 className="title_n2">{item.title}</h2>
+              </div>
+            </a>
+          ))}
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default Card;
