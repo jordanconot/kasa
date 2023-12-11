cbb6b8ef1bbf143beb2202ab5c3489c285a618b6
unknown <jordan.conot@gmail.com>
2023-12-11 22:37:38 +0100

    feat: add component location

--- src/components/LocationDetails.jsx
+++ src/components/LocationDetails.jsx
@@ -3,13 +3,51 @@ import data from '../data/data.json';
 import { useParams } from 'react-router-dom';
 
 const LocationDetails = () => {
-    const { id } = useParams();
-    const locationData = data.find(location => location.id === id);
-  
+  const { id } = useParams();
+  const locationData = data.find((location) => location.id === id);
 
-    return (  
-            <h1>{locationData.title}</h1>
-    );
+  const renderStars = (rating) => {
+    const starsActive = `../assets/star-active.svg`;
+    const starsInactive = `../assets/star-inactive.svg`;
+
+    let stars = [];
+    for(let i = 0; i < 5; i++) {
+        stars.push(
+            <img key={i} src={i < rating ? starsActive : starsInactive} alt='Note sur 5'></img>
+        )
+    }
+    return stars
+  }
+
+  return (
+    <main className="location_container">
+      <article className="card_location">
+        <img
+          className="card_location_picture"
+          src={locationData.cover}
+          alt={locationData.title}
+        ></img>
+      </article>
+      <div className="location_header_container">
+        <div className="location_header_title">
+          <h1>{locationData.title}</h1>
+          <p>{locationData.location}</p>
+          <div className="location_header_tag">
+            {locationData.tags.map((tag, index) => (
+                <span key={index} className="tag">{tag}</span>
+            ))}
+          </div>
+        </div>
+        <div className="location_header_star_host">
+          <div className="location_header_host">
+            <p>{locationData.host.name}</p>
+            <img src={locationData.host.picture} alt="blabla"></img>
+          </div>
+          <div className="location_header_star">{renderStars(Number(locationData.rating))}</div>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default LocationDetails;
--- src/pages/Location.jsx
+++ src/pages/Location.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import LocationDetails from '../components/LocationDetails';
+import Slideshow from '../components/Slideshow';
 
 const Location = () => {
     return (
         <div className='container'>
             <Header />
+            {/* <Slideshow /> */}
             <LocationDetails />
             <Footer />
         </div>
