d6400f549b74ecb38df5081e3b6dbf28574fba93
unknown <jordan.conot@gmail.com>
2023-12-14 13:07:20 +0100

    feat: remove arrow and cout if single image

--- src/components/LocationDetails.jsx
+++ src/components/LocationDetails.jsx
@@ -5,18 +5,6 @@ import { useParams } from 'react-router-dom';
 const LocationDetails = () => {
   const [currentPictureIndex, setCurrentPictureIndex] = useState(0);
 
-  const nextPicture = () => {
-    setCurrentPictureIndex((prevIndex) =>
-      prevIndex === locationData.pictures.length - 1 ? 0 : prevIndex + 1
-    );
-  };
-
-  const prevPicture = () => {
-    setCurrentPictureIndex((prevIndex) =>
-      prevIndex === 0 ? locationData.pictures.length - 1 : prevIndex - 1
-    );
-  };
-
   const { id } = useParams();
   const locationData = data.find((location) => location.id === id);
 
@@ -37,6 +25,20 @@ const LocationDetails = () => {
     return stars;
   };
 
+  const nextPicture = () => {
+    setCurrentPictureIndex((prevIndex) =>
+      prevIndex === locationData.pictures.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  const prevPicture = () => {
+    setCurrentPictureIndex((prevIndex) =>
+      prevIndex === 0 ? locationData.pictures.length - 1 : prevIndex - 1
+    );
+  };
+
+  const isSinglePicture = locationData.pictures.length === 1;
+
   return (
     <main className="location_container">
       <article className="card_location">
@@ -45,18 +47,23 @@ const LocationDetails = () => {
           src={locationData.pictures[currentPictureIndex]}
           alt={`${locationData.title} ${currentPictureIndex + 1}`}
         ></img>
-        <button
-          onClick={prevPicture}
-          className="card_location_arrow_left"
-        ></button>
-        <button
-          onClick={nextPicture}
-          className="card_location_arrow_right"
-        ></button>
-        <div className="card_location_counter">
-          {currentPictureIndex + 1} / {locationData.pictures.length}
-        </div>
+        {!isSinglePicture && (
+          <>
+            <button
+              onClick={prevPicture}
+              className="card_location_arrow_left"
+            ></button>
+            <button
+              onClick={nextPicture}
+              className="card_location_arrow_right"
+            ></button>
+            <div className="card_location_counter">
+              {currentPictureIndex + 1} / {locationData.pictures.length}
+            </div>
+          </>
+        )}
       </article>
+
       <div className="location_header_container">
         <div className="location_header_title">
           <h1>{locationData.title}</h1>
